f3aca5483623ad9d756c5faf39debed6f85a3f95
vivekvarma12 <33152570+vivekvarma12@users.noreply.github.com>
2021-02-07 19:21:04 +0530

    Update Apping.py
    Updated code

--- Apping.py
+++ Apping.py
@@ -1,32 +1,30 @@
 from tkinter import *
 import subprocess
+import xlrd
+data=xlrd.open_workbook(r"C:\Users\vidoddip\Desktop\Programs_data.xlsx")
+data1=data.sheet_by_index(0)
 def open():
     w=var.get()
-	    if w=="notepad" or w=="n++"  or w=="note++":
-	        subprocess.Popen([r"C:\Program Files (x86)\Notepad++\notepad++.exe"])
-		    elif w=="chrome" or w=="google" or w=="gchrome":
-			        subprocess.Popen([r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"])
-			    elif w=="fox" or w=="ffox" or w=="mfox":
-		        subprocess.Popen([r"C:\Program Files\Mozilla Firefox\firefox.exe"])
-					    elif w=="beans"  or w=="netb" or w=="nbeans":
-						        subprocess.Popen([r"C:\Program Files\NetBeans 8.2\bin\netbeans64.exe"])
-						    else:
-							        m=Tk()
-								    m.geometry("150x60")
-								    Label(m,text="enter a valid application").grid(row=1,column=3)
-								    m.mainloop()
-						        #print("Enter a valid application")
-
-menu=["n++","google","fox"]  #dropdown menu
+	    t=""
+	    for i in range(data1.nrows):
+		          if w == data1.cell_value(i,0):
+			            t=data1.cell_value(i,1)
+			            break
+	    subprocess.Popen(t)
+		
 master = Tk()
 master.title("cortona daddy")
 master.geometry("300x250")
 master.configure(bg="cyan")
+l=[]
+for i in range(data1.nrows):
+   l.append(data1.cell_value(i,0))
+
+
 var=StringVar(master)
-var.set(menu[0])
+var.set(l[0])
 Label(master,text="Give your Application name :",bg="cyan").grid(row=3,column=5)
-e=OptionMenu(master,var,*menu)
-#e=Entry(master)
+e=OptionMenu(master,var,*l)
 e.grid(row=3,column=6)
-Button(master, text='open', width=5, command=open).grid(row=4,column=6)
+Button(master, text='open', width=5, command=open).grid(row=5,column=6)
 master.mainloop()
